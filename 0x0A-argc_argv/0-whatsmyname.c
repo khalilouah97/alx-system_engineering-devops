d63aa8f073d9ba8ce68a511748bf19489fae1f82
khalil ouahmid <khalil.ouahmid@gmail.com>
2023-10-27 12:46:38 -0700

    f

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - If you rename the program, it will print the new name
+ *
+ * @argc: variable
+ * @argv: variable
+ * by : laila tabourit
+ *
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
